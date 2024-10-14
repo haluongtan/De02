11ce939a78889ee2fa5fd9c8a074785d1c78a4af
haluongtan <160831939+haluongtan@users.noreply.github.com>
2024-10-14 14:33:52 +0700

    Add files via upload

---
+++ ProductBUS.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL5.Entities;
+using DAL5;
+
+
+namespace BUS5
+{
+    public class ProductBUS
+    {
+        private ProductDAL productDAL;
+
+        public ProductBUS()
+        {
+            productDAL = new ProductDAL();
+        }
+
+        public List<Sanpham> GetAllProducts()
+        {
+            return productDAL.GetAllProducts();
+        }
+
+        public List<LoaiSP> GetAllCategories()
+        {
+            return productDAL.GetAllCategories();
+        }
+
+        public void AddOrUpdateProduct(Sanpham product)
+        {
+            var existingProduct = productDAL.GetAllProducts().FirstOrDefault(p => p.MaSP == product.MaSP);
+            if (existingProduct != null)
+            {
+                productDAL.UpdateProduct(product);
+            }
+            else
+            {
+                productDAL.AddProduct(product);
+            }
+        }
+
+        public void DeleteProduct(string maSP)
+        {
+            productDAL.DeleteProduct(maSP);
+        }
+
+        public string GetCategoryIdByName(string name)
+        {
+            var category = productDAL.GetCategoryByName(name);
+            return category?.MaLoai;
+        }
+        public List<Sanpham> GetProductsByName(string tenSP)
+        {
+            return productDAL.GetProductsByName(tenSP);
+        }
+
+
+    }
+}
+
---
+++ ProductDAL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL5.Entities;
+namespace DAL5
+{
+    public class ProductDAL
+    {
+        private QuanLySPModel db;
+
+        public ProductDAL()
+        {
+            db = new QuanLySPModel();
+        }
+
+        public List<Sanpham> GetAllProducts()
+        {
+            return db.Sanpham.Include("LoaiSP").ToList();
+        }
+
+        public List<LoaiSP> GetAllCategories()
+        {
+            return db.LoaiSP.ToList();
+        }
+
+        public void AddProduct(Sanpham product)
+        {
+            db.Sanpham.Add(product);
+            db.SaveChanges();
+        }
+
+        public void UpdateProduct(Sanpham product)
+        {
+            var existingProduct = db.Sanpham.SingleOrDefault(p => p.MaSP == product.MaSP);
+            if (existingProduct != null)
+            {
+                existingProduct.TenSP = product.TenSP;
+                existingProduct.NgayNhap = product.NgayNhap;
+                existingProduct.MaLoai = product.MaLoai;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(string maSP)
+        {
+            var product = db.Sanpham.SingleOrDefault(p => p.MaSP == maSP);
+            if (product != null)
+            {
+                db.Sanpham.Remove(product);
+                db.SaveChanges(); 
+            }
+        }
+
+        public LoaiSP GetCategoryByName(string name)
+        {
+            return db.LoaiSP.FirstOrDefault(c => c.TenLoai == name);
+        }
+        public List<Sanpham> GetProductsByName(string tenSP)
+        {
+            return db.Sanpham.Include("LoaiSP")
+                             .Where(p => p.TenSP.Contains(tenSP))
+                             .ToList();
+        }
+
+    }
+}
